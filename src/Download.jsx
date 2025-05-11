fe882568df43b0e66da272b7f4fa0d1ce94049e8
pearmini <subairui@icloud.com>
2025-05-10 23:26:00 -0400

    Optimize download page

--- src/Download.jsx
+++ src/Download.jsx
@@ -29,22 +29,54 @@ export function Download({text}) {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "flex-start",
+        overflow: "auto",
+        fontFamily: "monospace",
       }}
     >
-      <div ref={treeRef} style={{maxWidth: "calc(100vw - 30px)", maxHeight: "calc(100vh - 30px)"}}></div>
-      <div
-        style={{
-          display: "flex",
-          gap: "10px",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <button className="button" onClick={handleDownloadPNG}>
-          Download
-        </button>
+      <p style={{marginTop: "60px", fontSize: "20px", marginBottom: "20px"}}>Thanks for loving the tree!</p>
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+        <div ref={treeRef} style={{maxWidth: "calc(100vw - 30px)", maxHeight: "calc(100vh - 30px)"}}></div>
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <button className="button" onClick={handleDownloadPNG}>
+            Download
+          </button>
+        </div>
       </div>
+      <ul style={{fontSize: "18px", marginTop: "60px"}}>
+        <li style={{paddingBottom: "10px"}}>
+          <span>
+            Name2Tree: <a href="https://tree.bairui.dev">tree.bairui.dev</a>
+          </span>
+        </li>
+        <li style={{paddingBottom: "10px"}}>
+          <span>
+            APack: <a href="https://apack.bairui.dev">apack.bairui.dev</a>
+          </span>
+        </li>
+        <li style={{paddingBottom: "10px"}}>
+          <span>
+            Charming.js: <a href="https://charmingjs.org">charmingjs.org</a>
+          </span>
+        </li>
+        <li style={{paddingBottom: "10px"}}>
+          <span>
+            GitHub: <a href="https://github.com/pearmini">@pearmini</a>
+          </span>
+        </li>
+        <li style={{paddingBottom: "10px"}}>
+          <span>
+            Instagram: <a href="https://www.instagram.com/subairui24">@subairui24</a>
+          </span>
+        </li>
+      </ul>
     </div>
   );
 }
