b4ad8df2ca6ea85d39efe137a0530c026bfdc3f5
pearmini <subairui@icloud.com>
2025-08-03 15:55:10 -0400

    Refactor forest

--- src/Viz.jsx
+++ src/Viz.jsx
@@ -5,6 +5,8 @@ import {BACKGROUND_COLOR} from "./constants.js";
 export function Viz({names}) {
   const [loading, setLoading] = useState(false);
   const forestRef = useRef(null);
+  const layoutRef = useRef(null);
+  const [selectedLayout, setSelectedLayout] = useState("cloud");
   const headerHeight = 68;
 
   useEffect(() => {
@@ -12,13 +14,12 @@ export function Viz({names}) {
     if (forestRef.current) {
       setLoading(true);
       timeout = setTimeout(() => {
-        forestRef.current.innerHTML = "";
-        const node = forest(
-          names.map((d) => d.name),
-          {styleWidth: window.innerWidth, styleHeight: window.innerHeight - headerHeight},
-        );
-        // const node = root.render();
-        forestRef.current.appendChild(node);
+        layoutRef.current = forest(forestRef.current, {
+          styleWidth: window.innerWidth,
+          styleHeight: window.innerHeight - headerHeight,
+          names: names.map((d) => d.name),
+        });
+        layoutRef.current.update(selectedLayout);
         setLoading(false);
       }, 100);
     }
@@ -27,6 +28,12 @@ export function Viz({names}) {
     };
   }, [names, forestRef]);
 
+  useEffect(() => {
+    if (layoutRef.current) {
+      layoutRef.current.update(selectedLayout);
+    }
+  }, [selectedLayout]);
+
   return (
     <div style={{width: "100vw", height: "100vh", background: BACKGROUND_COLOR, overflow: "hidden"}}>
       <div
@@ -34,13 +41,24 @@ export function Viz({names}) {
           height: headerHeight,
           borderBottom: "1px solid #ccc",
           display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
+          flexDirection: "row",
+          justifyContent: "space-between",
           padding: "0 20px",
+          paddingRight: 100,
         }}
       >
-        <h2 style={{marginBottom: 4}}>Tree Cloud: Placing trees along a spiral path</h2>
-        <p>Pan and zoom. Click to zoom to a specific tree.</p>
+        <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+          <h2 style={{marginBottom: 4}}>Tree Cloud: Placing trees along a spiral path</h2>
+          <p>Pan and zoom. Click to zoom to a specific tree.</p>
+        </div>
+        <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+          <div className="select-container">
+            <select className="select" value={selectedLayout} onChange={(e) => setSelectedLayout(e.target.value)}>
+              <option value="cloud">Cloud</option>
+              <option value="grid">Grid</option>
+            </select>
+          </div>
+        </div>
       </div>
       <div>
         {loading && (
