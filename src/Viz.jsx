7d4f893b0acb749e79d9270f3ef5a62109188169
pearmini <subairui@icloud.com>
2025-08-04 18:19:28 -0400

    Add grid viz

--- src/Viz.jsx
+++ src/Viz.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useRef, useState, useMemo} from "react";
 import {forest} from "./drawForest.js";
 import {BACKGROUND_COLOR} from "./constants.js";
 
@@ -6,33 +6,55 @@ export function Viz({names}) {
   const [loading, setLoading] = useState(false);
   const forestRef = useRef(null);
   const layoutRef = useRef(null);
-  const [selectedLayout, setSelectedLayout] = useState("cloud");
+  const [selectedLayout, setSelectedLayout] = useState("grid");
+  const [sortBy, setSortBy] = useState("time-asc");
   const headerHeight = 68;
 
+  const description = useMemo(() => {
+    if (selectedLayout === "cloud") {
+      return {
+        title: "Tree Cloud: Placing trees along a spiral path",
+        description: "Pan and zoom. Click to zoom to a specific tree.",
+      };
+    } else if (selectedLayout === "grid") {
+      return {
+        title: "Tree Grid: Placing trees in a grid",
+        description: "Pan and zoom. Click to zoom to a specific tree.",
+      };
+    }
+  }, [selectedLayout]);
+
   useEffect(() => {
-    let timeout;
     if (forestRef.current) {
-      setLoading(true);
-      timeout = setTimeout(() => {
-        layoutRef.current = forest(forestRef.current, {
-          styleWidth: window.innerWidth,
-          styleHeight: window.innerHeight - headerHeight,
-          names: names.map((d) => d.name),
-        });
-        layoutRef.current.update(selectedLayout);
-        setLoading(false);
-      }, 100);
+      const filteredNames = names.filter((d) => d.createdAt && d.id);
+
+      layoutRef.current = forest(forestRef.current, {
+        styleWidth: window.innerWidth,
+        styleHeight: window.innerHeight - headerHeight,
+        names: filteredNames,
+      });
+
+      layoutRef.current.on("start", () => setLoading(true));
+      layoutRef.current.on("end", () => setLoading(false));
+
+      layoutRef.current.update({layout: selectedLayout, sortBy});
     }
     return () => {
-      clearTimeout(timeout);
+      layoutRef.current.stop();
     };
   }, [names, forestRef]);
 
   useEffect(() => {
     if (layoutRef.current) {
-      layoutRef.current.update(selectedLayout);
+      layoutRef.current.update({layout: selectedLayout, sortBy});
     }
   }, [selectedLayout]);
+
+  useEffect(() => {
+    if (layoutRef.current) {
+      layoutRef.current.update({layout: selectedLayout, sortBy});
+    }
+  }, [sortBy]);
 
   return (
     <div style={{width: "100vw", height: "100vh", background: BACKGROUND_COLOR, overflow: "hidden"}}>
@@ -48,10 +70,22 @@ export function Viz({names}) {
         }}
       >
         <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
-          <h2 style={{marginBottom: 4}}>Tree Cloud: Placing trees along a spiral path</h2>
-          <p>Pan and zoom. Click to zoom to a specific tree.</p>
+          <h2 style={{marginBottom: 4}}>{description.title}</h2>
+          <p>{description.description}</p>
         </div>
-        <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: 20}}>
+          {selectedLayout === "grid" && (
+            <div className="select-container">
+              <select className="select" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                <option value="time-asc">Sort by Time (Asc)</option>
+                <option value="time-desc">Sort by Time (Desc)</option>
+                <option value="name-asc">Sort by Name (Asc)</option>
+                <option value="name-desc">Sort by Name (Desc)</option>
+                <option value="number-asc">Sort by Digits (Asc)</option>
+                <option value="number-desc">Sort by Digits (Desc)</option>
+              </select>
+            </div>
+          )}
           <div className="select-container">
             <select className="select" value={selectedLayout} onChange={(e) => setSelectedLayout(e.target.value)}>
               <option value="cloud">Cloud</option>
@@ -69,7 +103,11 @@ export function Viz({names}) {
               left: "50%",
             }}
           >
-            <div style={{transform: "translate(-50%, -50%)"}}>Rendering...</div>
+            <div
+              style={{transform: "translate(-50%, -50%)", background: BACKGROUND_COLOR, padding: 10, borderRadius: 4}}
+            >
+              Rendering...
+            </div>
           </div>
         )}
         <div ref={forestRef}></div>
--- src/drawForest.js
+++ src/drawForest.js
@@ -24,7 +24,7 @@ function randomize(nodes) {
       .split("")
       .map((c) => c.charCodeAt(0))
       .join("");
-    const random = d3.randomUniform.source(d3.randomLcg(+seed));
+    const random = d3.randomUniform.source(d3.randomLcg(+seed * 1000));
     const r = random(-240, 240);
     d.x += r();
     d.y += r();
@@ -83,9 +83,12 @@ function cloud(nodes, {increase = 10, padding = 20} = {}) {
 }
 
 export function forest(container, {styleWidth, styleHeight, names}) {
-  const cells = names.map((d) => ({width: 480, height: 480, x: 0, y: 0, name: d}));
+  const cells = names.map((d) => ({width: 480, height: 480, x: 0, y: 0, ...d}));
+  const listeners = {};
 
-  function render() {
+  let timeout;
+
+  function render({cells, line = false} = {}) {
     container.innerHTML = "";
 
     const {minX, minY, maxX, maxY} = extend(cells);
@@ -173,7 +176,7 @@ export function forest(container, {styleWidth, styleHeight, names}) {
       .attr("transform", (d) => `translate(${d.x - 240}, ${d.y - 240})`);
 
     grids.append("g").each(function (d, i) {
-      const treeNode = tree(d.name, {padding: 0, number: false, line: false, end: false});
+      const treeNode = tree(d.name, {padding: 0, number: false, line, end: false});
       this.appendChild(treeNode.render());
     });
 
@@ -192,17 +195,87 @@ export function forest(container, {styleWidth, styleHeight, names}) {
     cloud(cells);
     randomize(cells);
     scaleY(cells, styleWidth / styleHeight);
-    render();
+    render({cells});
   }
 
-  function grid() {
-    container.innerHTML = "";
+  function grid({sortBy}) {
+    const getNumber = (d) => {
+      const text = d.name;
+      const ascii = text
+        .split("")
+        .map((code) => code.charCodeAt(0))
+        .join("");
+      const digitsSum = ascii.split("").reduce((acc, digit) => acc + +digit, 0);
+      return digitsSum;
+    };
+
+    const numberedCells = cells.map((d, i) => ({...d, number: getNumber(d)}));
+
+    let comparator;
+
+    switch (sortBy) {
+      case "time-asc":
+        comparator = (a, b) => d3.ascending(a.createdAt, b.createdAt);
+        break;
+      case "time-desc":
+        comparator = (a, b) => d3.descending(a.createdAt, b.createdAt);
+        break;
+      case "name-asc":
+        comparator = (a, b) => d3.ascending(a.name, b.name);
+        break;
+      case "name-desc":
+        comparator = (a, b) => d3.descending(a.name, b.name);
+        break;
+      case "number-asc":
+        comparator = (a, b) => d3.ascending(a.number, b.number);
+        break;
+      case "number-desc":
+        comparator = (a, b) => d3.descending(a.number, b.number);
+        break;
+      default:
+        comparator = (a, b) => d3.ascending(a.createdAt, b.createdAt);
+        break;
+    }
+
+    const sortedCells = d3.sort(numberedCells, comparator);
+
+    const t = styleHeight / styleWidth;
+    const n = Math.ceil(Math.sqrt(sortedCells.length / t));
+
+    for (let i = 0; i < sortedCells.length; i++) {
+      const x = (i % n) * 480;
+      const y = Math.floor(i / n) * 480;
+      sortedCells[i].x = x;
+      sortedCells[i].y = y;
+    }
+
+    render({line: true, cells: sortedCells});
   }
 
-  function update(layout) {
-    if (layout === "cloud") wordCloud();
-    else if (layout === "grid") grid();
+  function update({layout, sortBy}) {
+    emit("start");
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      if (layout === "cloud") wordCloud();
+      else if (layout === "grid") grid({sortBy});
+      emit("end");
+    }, 50);
   }
 
-  return {update};
+  function on(event, callback) {
+    if (!listeners[event]) listeners[event] = [];
+    listeners[event].push(callback);
+  }
+
+  function emit(event, ...args) {
+    if (listeners[event]) {
+      listeners[event].forEach((callback) => callback(...args));
+    }
+  }
+
+  function stop() {
+    clearTimeout(timeout);
+  }
+
+  return {update, on, stop};
 }
